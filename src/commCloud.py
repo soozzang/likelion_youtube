a8900a32ac3b7f013e3d47353a777606c6bdb403
kseysh <kseysh123@naver.com>
2023-04-07 17:53:16 +0900

    주석 지웠습니다.

--- src/commCloud.py
+++ src/commCloud.py
@@ -1,12 +1,7 @@
-'''
-commcloud - Comment Cloud
-YouTube 영상의 댓글 데이터를 가져와서, 명사만 추출하여 wordcloud로 시각화하기
-'''
-
 import konlpy
-from konlpy.tag import Okt          # 한글 텍스트 처리 및 형태소 분석기
-from collections import Counter     # 빈도수 계산
-from youtube import YoutubeAPI        # youtube.py 파일에서 comments 리스트 불러오기
+from konlpy.tag import Okt       
+from collections import Counter 
+from youtube import YoutubeAPI   
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 
@@ -20,26 +15,22 @@ class Cloud(YoutubeAPI):
 
     def __morpheme_analysis(self):
         okt = Okt()
-        #형태소 분석을 해주는 okt 객체를 생성
-        nouns = list()                      # 명사 추출 후 담을 빈 리스트 생성
+        nouns = list()             
         for comment in self.comments:
             for noun in okt.nouns(comment[0]):
-                # okt.nouns() : 문장에서 명사만 추출하는 함수 입력으로 들어온 문장에서 명사만 추출하여 리스트 형태로 반환
-                if len(noun) > 1:           # 명사의 길이가 1보다 작은 경우 리스트에 append하지 않음
+                if len(noun) > 1:      
                     nouns.append(noun)
 
-        count = Counter(nouns)              # 명사의 빈도 수 계산
+        count = Counter(nouns)       
         tags = count.most_common(50) 
-       # 빈도 수가 높은 50개의 명사를 리스트에 저장
         self.__generate_wordCloud(tags)
 
     def __generate_wordCloud(self,tags):
-        # 단어구름 생성
-        wc = WordCloud(width=800, height=600, background_color='white', font_path='font/NanumSquareRoundB.ttf')   # 사용할 폰트 경로 기입
-        cloud = wc.generate_from_frequencies(dict(tags))#단어구름 생성
+        wc = WordCloud(width=800, height=600, background_color='white', font_path='font/NanumSquareRoundB.ttf')   
+        cloud = wc.generate_from_frequencies(dict(tags))
 
-        plt.figure(figsize=(10, 8))     # 출력할 크기
-        plt.axis('off')                    #x,y축의 눈금과 라벨을 제거합니다.
-        plt.imshow(cloud)             # 생성된 단어구름 이미지 생성 준비
-        plt.show()                      #생성된 이미지 출력
+        plt.figure(figsize=(10, 8))     
+        plt.axis('off')                  
+        plt.imshow(cloud)            
+        plt.show()               
 
--- src/youtube.py
+++ src/youtube.py
@@ -5,25 +5,16 @@ from googleapiclient.discovery import build
     
 class YoutubeAPI:
     def __init__(self):
-        self.__api_key = os.environ.get('GOOGLE_API_KEY') #코드상에서 키를 숨기기위해 환경변수를 이용하여 GOOGLE_API_KEY라는 변수안에 키를 담아둔 후 호출.
+        self.__api_key = os.environ.get('GOOGLE_API_KEY')
         self.__api_obj = build('youtube', 'v3', developerKey=self.__api_key)
         self.__video_id=""
         self.comments = list()
-
-        #build 함수를 통해 Google API 클라이언트에서 지원하는 서비스를 사용할 수 있는 API 서비스 객체를 생성
-        #build 함수를 실행시키기 위해 API 서비스의 이름, API의 버전, 필요한 인증정보 제공
 
     def _get_comment_list(self):
         while True:
             try:
                 self.video_id = input("동영상 ID를 입력하세요: ")
-                responseComment = self.__api_obj.commentThreads().list(part='snippet', videoId=self.video_id, maxResults=100).execute()
-                #commentThreads : 특정 동영상에 연결된 댓글 스레드(thread) 목록을 검색하는 데 사용
-                #위 코드에서 part 매개변수는 API 응답에 포함되어야 할 리소스의 일부를 지정합니다. 
-                #여기서는 댓글 스레드와 관련된 정보를 모두 포함하도록 snippet으로 설정하였습니다. 
-                #replies는 해당 댓글 스레드에 포함된 모든 댓글과 답글 목록을 가져올 수 있다. 댓글의 대댓글은 불필요하므로 지웠습니다!
-                #videoId 매개변수는 검색하려는 동영상의 ID를 지정합니다.
-                #commentThreads() 함수의 반환값은 JSON 형식으로 제공되며, 검색된 댓글 스레드 목록을 포함합니다. 반환된 JSON에서 필요한 정보를 추출하여 사용할 수 있습니다.
+                responseComment = self.__api_obj.commentThreads().list(part='snippet', videoId=self.video_id, maxResults=100).execute()        
                 break
             except:
                 print("존재하지 않는 동영상입니다. 다시 입력해주세요.") 
@@ -33,39 +24,26 @@ class YoutubeAPI:
         response = self.__api_obj.videos().list(
         part='snippet,statistics',
         id=self.video_id
-        ).execute() # 서버로 api 요청//video info
-        #statistics : 동영상의 통계 정보 요청시 사용, 영상의 조회수, 좋아요 수, 싫어요 수 등의 정보 포함
-        #contentDetails : 동영상의 콘텐츠 상세 정보를 요청시 사용, 동영상의 길이, 해상도, 썸네일 URL 등의 정보 포함
-        #id : 동영상의 ID를 요청할 때 사용
-        self.__video_info(response)
+        ).execute() 
+        self.__print_info(response)
         self.__print_comment(responseComment)
         
     
-    def __video_info(self,response): # video의 정보들 뽑아내는 함수.
-        for item in response['items']: #for 루프를 사용하여 딕셔너리 형태로 저장된 정보에 접근
+    def __print_info(self,response):
+        for item in response['items']: 
             print("제목:", item['snippet']['title']+"\n")
             print("조회수:", item['statistics']['viewCount']+"\n")
             print("설명:", item['snippet']['description']+"\n")
             print("채널명:", item['snippet']['channelTitle']+"\n")
 
     def __print_comment(self,responseComment):
-        while responseComment:  # 댓글이 다음페이지에도 있다면 계속 돌리고 , 이제 없으면 break
-            for item in responseComment['items']: # items(댓글데이터)를 받아서 comments list에 추가
+        while responseComment:  
+            for item in responseComment['items']: 
                 comment = item['snippet']['topLevelComment']['snippet']
-                #topLevelComment : 해당 동영상의 최상위 댓글을 나타내는 객체로 객체 안에 댓글 작성자, 댓글 내용, 작성 시간 등의 정보가 담김
                 self.comments.append([comment['textDisplay']])
-                #textDisplay : 댓글 작성자가 본문에 HTML 태그를 사용한 경우, textDisplay 속성에는 HTML 태그가 포함되지 않은 일반 텍스트 형식으로z
         
-            if 'nextPageToken' in responseComment: #댓글이 다음페이지에 있다면 넘겨서 댓글을 더 가져옴.
-                responseComment = self.__api_obj.commentThreads().list(part='snippet', videoId=self.video_id, pageToken=responseComment['nextPageToken'], maxResults=100).execute()
-                #기본적으로 API는 한 번에 최대 100개의 댓글 스레드를 반환한다. 
-                # 만약 전체 댓글 스레드가 100개 이상이라면, PageToken매개변수를 사용하여 다음 페이지의 결과를 요청해야함
-                #nextPageToken은 이전 페이지에서 반환된 마지막 댓글 스레드의 토큰입니다. 
-                # 이 토큰을 pageToken 매개변수에 지정하여 다음 페이지를 요청하면 됩니다.
-                #commentThreads API에서 maxResults 매개변수는 한 페이지에 반환할 최대 결과 수를 나타냅니다.
-                #기본값으로는 20이 설정되어 있지만, 최대 100까지 설정할 수 있습니다.
-                #반환되는 댓글 쓰레드가 100개 미만이라면 nextPageToken이 null값을 가진다.
-            
+            if 'nextPageToken' in responseComment:
+                responseComment = self.__api_obj.commentThreads().list(part='snippet', videoId=self.video_id, pageToken=responseComment['nextPageToken'], maxResults=100).execute()      
             else:
                 break
  
