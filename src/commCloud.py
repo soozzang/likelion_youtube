01063a6e83bd7a844eed71e8579c17e1868de509
lavenderize <liexquisit@gmail.com>
2023-04-07 10:08:42 +0900

    readme.md 수정

--- src/commCloud.py
+++ src/commCloud.py
@@ -1,23 +1,24 @@
 '''
+commcloud - Comment Cloud
 YouTube 영상의 댓글 데이터를 가져와서, 명사만 추출하여 wordcloud로 시각화하기
 '''
 
 import konlpy
 from konlpy.tag import Okt          # 한글 텍스트 처리 및 형태소 분석기
 from collections import Counter     # 빈도수 계산
-from youtube import comments            # asd.py 파일에서 comments 리스트 불러오기
+from youtube import comments        # youtube.py 파일에서 comments 리스트 불러오기
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 
 okt = Okt()
-nouns = list()      # 명사 추출 후 담을 빈 리스트 생성
+nouns = list()                      # 명사 추출 후 담을 빈 리스트 생성
 for comment in comments:
     for noun in okt.nouns(comment[0]):
-        if len(noun) > 1:   # 명사의 길이가 1보다 작은 경우 리스트에 append하지 않음
+        if len(noun) > 1:           # 명사의 길이가 1보다 작은 경우 리스트에 append하지 않음
             nouns.append(noun)
 
-count = Counter(nouns)          # 명사의 빈도 수 계산
-tags = count.most_common(50)    # 빈도 수가 높은 50개의 명사를 리스트에 저장
+count = Counter(nouns)              # 명사의 빈도 수 계산
+tags = count.most_common(50)        # 빈도 수가 높은 50개의 명사를 리스트에 저장
 
 # 단어구름 생성
 wc = WordCloud(width=800, height=600, background_color='white', font_path='font/NanumSquareRoundB.ttf')   # 사용할 폰트 경로 기입
